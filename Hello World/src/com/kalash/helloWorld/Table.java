baf6d339a6ecbdd4a9f17723b5a8d25601fd3af9
Kalash Jain <103770488+jainkalash@users.noreply.github.com>
2023-07-11 16:00:00 +0530

    Add files via upload

---
+++ Hello World/src/com/kalash/helloWorld/Table.java
@@ -0,0 +1,12 @@
+package com.kalash.helloWorld;
+
+public class Table {
+
+	public static void main(String[] args) {
+		MultiplicationTable mt=new MultiplicationTable();
+		//mt.print();
+		//mt.print(6);
+		mt.print(7,1,10);
+	}
+
+}
